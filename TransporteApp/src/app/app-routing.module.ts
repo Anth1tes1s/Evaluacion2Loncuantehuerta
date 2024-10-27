27e25342ca7f41414079a5207279197662ea2390
Anth1tes1s <ca.loncuante@duocuc.cl>
2024-10-27 17:09:54 -0600

    Agregado de funciones de Google GEO (API) y inicio usuarios
    Agregado de inicio usuarios

--- TransporteApp/src/app/app-routing.module.ts
+++ TransporteApp/src/app/app-routing.module.ts
@@ -3,46 +3,42 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'inicio', // Página de inicio predeterminada
     pathMatch: 'full'
   },
   {
     path: 'inicio',
-    loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
-  },
-  {
-    path: 'iniciousuario',
-    loadChildren: () => import('./pages/iniciousuario/iniciousuario.module').then( m => m.IniciousuarioPageModule)
-  },
-  {
-    path: 'inicioconductor',
-    loadChildren: () => import('./pages/inicioconductor/inicioconductor.module').then( m => m.InicioconductorPageModule)
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'recuperarcontrasnia',
-    loadChildren: () => import('./pages/recuperarcontrasnia/recuperarcontrasnia.module').then( m => m.RecuperarcontrasniaPageModule)
+    loadChildren: () => import('./pages/inicio/inicio.module').then(m => m.InicioPageModule)
   },
   {
     path: 'registro',
-    loadChildren: () => import('./pages/registro/registro.module').then( m => m.RegistroPageModule)
+    loadChildren: () => import('./pages/registro/registro.module').then(m => m.RegistroPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: 'recuperarcontrasnia',
+    loadChildren: () => import('./pages/recuperarcontrasnia/recuperarcontrasnia.module').then(m => m.RecuperarcontrasniaPageModule)
+  },
+  {
+    path: 'iniciousuario',
+    loadChildren: () => import('./pages/iniciousuario/iniciousuario.module').then(m => m.IniciousuarioPageModule)
   },
   {
     path: 'registroconductor',
-    loadChildren: () => import('./pages/registroconductor/registroconductor.module').then( m => m.RegistroconductorPageModule)
+    loadChildren: () => import('./pages/registroconductor/registroconductor.module').then(m => m.RegistroconductorPageModule)
+  },
+  {
+    path: 'inicioconductor',
+    loadChildren: () => import('./pages/inicioconductor/inicioconductor.module').then(m => m.InicioconductorPageModule)
   },
   {
     path: 'servicio',
     loadChildren: () => import('./pages/servicio/servicio.module').then( m => m.ServicioPageModule)
-  },
+  }, 
   {
     path: 'servicioconductor',
     loadChildren: () => import('./pages/servicioconductor/servicioconductor.module').then( m => m.ServicioconductorPageModule)
@@ -56,3 +52,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- TransporteApp/src/app/services/conductor.service.ts
+++ TransporteApp/src/app/services/conductor.service.ts
@@ -4,6 +4,22 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ConductorService {
+  private conductorSeleccionado: any;
 
-  constructor() { }
+  constructor() {}
+
+  // Método para seleccionar un conductor y guardar su información
+  seleccionarConductor(conductor: any) {
+    this.conductorSeleccionado = conductor;
+  }
+
+  // Método para obtener la información del conductor seleccionado
+  obtenerConductorSeleccionado() {
+    return this.conductorSeleccionado;
+  }
+
+  // Método para liberar el viaje, eliminando la información del conductor
+  liberarViaje() {
+    this.conductorSeleccionado = null;
+  }
 }
