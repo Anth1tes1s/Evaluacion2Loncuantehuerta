e5ad269cbd1171d0fdc2a99bc89742daa1720bfd
Anth1tes1s <ca.loncuante@duocuc.cl>
2024-10-27 16:34:59 -0600

    creación de inicio, registro, login y servicio.
    Creación de servicio de validaciones para el registro y login.

--- TransporteApp/src/app/pages/login/login.page.ts
+++ TransporteApp/src/app/pages/login/login.page.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { VerificacionService } from '../../services/verificacion.service';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router'; // Importa Router
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
 
-  constructor() { }
+  usuario: string = ''; // Inicializar como cadena vacía
+  contrasenia: string = ''; // Inicializar como cadena vacía
 
-  ngOnInit() {
+  constructor(private verificacionService: VerificacionService, private alertController: AlertController, private router: Router) {}
+
+  async iniciarSesion() {
+    // Validar que los campos no estén vacíos
+    if (!this.usuario || !this.contrasenia) {
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Por favor, complete los campos.',
+        buttons: ['OK']
+      });
+      await alert.present();
+      return; // Detener la ejecución si hay campos vacíos
+    }
+
+    const usuarios = this.verificacionService.obtenerUsuarios();
+    const usuarioEncontrado = usuarios.find(user => user.usuario === this.usuario && user.contrasenia === this.contrasenia);
+
+    if (usuarioEncontrado) {
+      const alert = await this.alertController.create({
+        header: 'Bienvenido',
+        message: 'Usuario ingresado correctamente',
+        buttons: [{
+          text: 'OK',
+          handler: () => {
+            this.router.navigate(['/iniciousuario']); // Redirigir al usuario a la página de inicio
+          }
+        }]
+      });
+      await alert.present();
+    } else {
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Ingreso de datos incorrectos, inténtelo nuevamente.',
+        buttons: ['OK']
+      });
+      await alert.present();
+    }
   }
-
 }
+
