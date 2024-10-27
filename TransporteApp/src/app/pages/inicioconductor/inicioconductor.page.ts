882ec3a8b21b55a28942c4323010413144fe7812
PLAYDEI <87295691+PLAYDEI@users.noreply.github.com>
2024-10-27 17:39:51 -0300

    hice inicio conductor
    a

--- TransporteApp/src/app/pages/inicioconductor/inicioconductor.page.ts
+++ TransporteApp/src/app/pages/inicioconductor/inicioconductor.page.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicioconductor',
   templateUrl: './inicioconductor.page.html',
   styleUrls: ['./inicioconductor.page.scss'],
 })
-export class InicioconductorPage implements OnInit {
+export class InicioconductorPage {
+  viajesDisponibles = [
+    { destino: 'Aeropuerto', cliente: 'Juan Pérez', hora: '14:30', origen: 'Plaza de Armas', distancia: 20 },
+    { destino: 'Centro Comercial', cliente: 'María López', hora: '15:00', origen: 'Estación Central', distancia: 15 },
+    { destino: 'Hospital', cliente: 'Pedro González', hora: '15:30', origen: 'Universidad de Chile', distancia: 10 }
+  ];
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+  seleccionarViaje(viaje: any) {
+    this.router.navigate(['/servicioconductor'], { state: { viaje } });
   }
-
 }
+
